46311a37bbc29461e9f475a46a5acca42a6f556c
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-03-21 16:29:44 -0400

    Initial comment for hello world application

---
+++ Readme.md
@@ -0,0 +1,13 @@
+﻿# Hello World .NET Core Application
+
+# Run locally
+```
+dotnet run --project hello-world-api
+```
+Then launch your browser and access http://localhost:5000/api/hello
+
+# Deploy to Cloud Foundry
+```
+cf push
+```
+Follow the console output to gather the url
---
+++ hello-world-api/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_world_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET api/helllo
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+
+    }
+}
